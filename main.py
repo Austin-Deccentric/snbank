0f80a00d768c55a882b9c06cd8fc143c0f753bb8
Austin-Deccentric <caugust19.ac@gmail.com>
2020-05-07 23:55:29 +0200

    Implemented all requirements

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import itertools
 from datetime import datetime
 import time
+from random import choices, seed
+import string
+from os import remove
+import sys
 
 def login():
     condition = True
@@ -10,9 +14,7 @@ def login():
         password = input("Please enter password: ").strip()
         with open('staff.txt') as f:
             for line1,line2 in itertools.zip_longest(*[f]*2):
-                #print(line1,line2)
                 if line1.startswith('Username'):
-                    #print(line1.strip().split(' ')[1],line2.strip().split(' ')[1])
                     name = line1.strip().split(' ')[1]
                     passkey = line2.strip().split(' ')[1]
                     if username.lower()==name.lower() and (password ==passkey):
@@ -21,7 +23,6 @@ def login():
                         break
             else:
                 print("Invalid username or password")
-                    #print("Invalid Username or password")
     return username
     
 def session_file(user):
@@ -37,17 +38,52 @@ while True:
     user_input = input('''Please select an option:
 1 Staff Login
 2 Close App
->''')
+>''').strip()
 
     if user_input not in ['1','2']:
         print('Enter a valid option')
         continue
     else:
         user_input = int(user_input)
-        break
+        
 
-if user_input == 1:
-    username = login()
-    session_file(username)
-    # print("Welcome")
-    
+    if user_input == 1:
+        username = login()
+        session_file(username)
+        actions = True
+        while actions != '3':
+            actions = input('''Please select an Option
+1. Create a new account
+2. Check account details
+3. Logout
+> ''').strip()
+            if actions == '1':
+                acc_name = input("Enter account holder's name: ")
+                opening_balance = int(input('Enter Opening Balance: '))     
+                acc_type = input("Enter account type: ")
+                acc_email = input("Enter Account email")
+                seed(datetime.now())
+                acc_num = ''.join(choices(string.digits,k = 10))
+                with open('customer.txt', 'a') as file:
+                    file.write('{}: {}, {}, {}, {}'.format(acc_num,acc_name,opening_balance,acc_type,acc_email))
+                    file.write('\n')
+                print("Your account number is: ",acc_num)
+                continue
+
+            if actions == '2':
+                acc_num = input("Enter account number: ").strip()
+                with open('customer.txt', 'r') as file:
+                    for line in file:
+                        if line.startswith(acc_num):
+                            acc_details = line.strip().split(' ',1)[1].split(',')
+                            print(f'''Account Name: {acc_details[0]}
+Balance:{acc_details[1]}
+Account type:{acc_details[2]}
+Email:{acc_details[3]}
+''')
+            if actions == '3':
+                remove('log.txt')
+
+        continue
+    if user_input == 2:
+        sys.exit()
--- test.py
+++ test.py
@@ -1,8 +1,24 @@
-# user = "BuchiCorp"
-# pascode='234@pagang'
-with open('staff.txt') as f:
-        for line in f:
-                #print(line)
-                username, details= line.strip().split(':',maxsplit=1)
-                print(username)
-                print(details.strip())
+# # user = "BuchiCorp"
+# # pascode='234@pagang'
+# with open('staff.txt') as f:
+#         for line in f:
+#                 #print(line)
+#                 username, details= line.strip().split(':',maxsplit=1)
+#                 print(username)
+#                 print(details.strip())
+
+# from random import seed, choices
+# from datetime import datetime
+# import string
+
+acc_num = input("Enter account number: ").strip()
+with open('customer.txt', 'r') as file:
+    for line in file:
+        if line.startswith(acc_num):
+            acc_details = line.strip().split(' ',1)[1].split(',')
+            
+            print(f'''Account Name: {acc_details[0]}
+Balance:{acc_details[1]}
+Account type:{acc_details[2]}
+Email:{acc_details[3]}
+''')
